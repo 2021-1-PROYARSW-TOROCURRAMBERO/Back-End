b19e026aed31c38ed0dd6471049fd861df10847e
Juan Sebastian <Juan_munozd@hotmail.es>
2021-05-11 16:55:36 -0500

    update library WebMvcConfigurer

--- src/main/java/edu/eci/arsw/quickmobility/config/WebConfig.java
+++ src/main/java/edu/eci/arsw/quickmobility/config/WebConfig.java
@@ -8,12 +8,11 @@ package edu.eci.arsw.quickmobility.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter{
+public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");;
